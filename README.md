6843fba23928dbe32034c925d670b05eafb2dc3a
Daniel Schaub <danielp.schaub@gmail.com>
2026-04-14 11:57:11 -0600

    Uploaded the component code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Svelte5_TextareaWithCount
 There aren't many options out there for svelte components. This is a textarea component that comes with an auto-updating letter count in the bottom right.
+
+## How to use:
+1. Place it in somewhere like `src/lib/components/TextareaWithCount.svelte`
+2. Access it with `import TextareaWithCount from '$lib/components/TextareaWithCount.svelte`
+
+## Styling
+I used Tailwind CSS for some basic styling, but you don't have to use that by any means. Just replace the classes with a styling format you're familiar with - the layouts are very basic.
---
+++ TextareaWithCount.svelte
@@ -0,0 +1,25 @@
+<script>
+  let { name, value="", rows='2', maxlength='200', placeholder='', disabled=false } = $props();
+  let currentChars = $state(value.length);
+  function getLength(){
+    currentChars = value.length;
+  }
+</script>
+
+<div class="flex flex-col w-full">
+  <textarea
+    id={name}
+    bind:value={value}
+    name={name}
+    oninput={getLength}
+    rows={rows}
+    maxlength={maxlength}
+    placeholder={placeholder}
+    disabled={disabled}
+    class=""
+  ></textarea>
+  <p class="text-xs text-right">{currentChars}/{maxlength}</p>
+</div>
+
+<style>
+</style>
